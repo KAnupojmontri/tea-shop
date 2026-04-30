89bdaa32192831f5e4a2a2c2486036fe78f9a5e8
kbanpmt <k.anupojmontri@gmail.com>
2026-04-30 15:56:42 +0700

    feat : style component

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Counter from './learn-hooks/Counter'
-import SimpleForm from './learn-hooks/SimpleForm'
-import WereWolf from './learn-hooks/Werewolf'
-import TimerCounter from './learn-hooks/TimerCounter'
-import FocusInput from './learn-hooks/FocusInput'
-import CounterWithRef from './learn-hooks/CounterWithRef'
-import Stopwatch from './learn-hooks/Stopwatch'
-import ExpensiveComponent from './learn-hooks/ExpensiveComponent'
-import MemoizedComponent from './learn-hooks/MemoizedComponent'
-import UserProfile from './learn-hooks/UserProfile'
+import "./index.css"
+import Tailwind from './style-component/Tailwind'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Counter></Counter>
-    <SimpleForm></SimpleForm>
-    <WereWolf></WereWolf>
-    <TimerCounter></TimerCounter>
-    <FocusInput></FocusInput>
-    <CounterWithRef></CounterWithRef>
-    <Stopwatch></Stopwatch>
-    <ExpensiveComponent></ExpensiveComponent>
-    <MemoizedComponent></MemoizedComponent>
-    <UserProfile></UserProfile>
+    <Tailwind></Tailwind>
   </StrictMode>,
 )
---
+++ src/style-component/Tailwind.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tailwind() {
+  return (
+    <div className='bg-red-400 p-4'>Tailwind</div>
+  )
+}
+
+export default Tailwind
